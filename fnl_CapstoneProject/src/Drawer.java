f9b6674d88f9e820f7c680ea83f673a59b15fe88
Hayden Floyd <144164954+hfloyd501@users.noreply.github.com>
2024-05-03 11:52:19 -0700

    b

---
+++ fnl_CapstoneProject/src/Drawer.java
@@ -0,0 +1,20 @@
+
+import java.awt.Color;
+import java.awt.Graphics;
+//import java.awt.event.ActionEvent;
+//import java.awt.event.ActionListener;
+
+import javax.swing.JPanel;
+import javax.swing.Timer;
+
+public class Drawer extends JPanel //implements ActionListener
+{
+
+	public void setBackground(Color white) {
+		// TODO Auto-generated method stub
+		
+		
+	}
+	
+
+}
--- fnl_CapstoneProject/src/PongTest.java
+++ fnl_CapstoneProject/src/PongTest.java
@@ -1,5 +1,11 @@
 import javax.swing.JFrame;
-public class PongTest {
+
+import java.awt.Color;
+import java.awt.Graphics;
+
+
+public class PongTest 
+{
 	boolean startGame = true;
 	int score1 = 0, score2 = 0;
 	static final int WIDTH = 1280, HEIGHT = 1280;
@@ -29,7 +35,14 @@ public class PongTest {
 		
 	}
  	public static void main(String[] args) {
-		
+		JFrame window = new JFrame("Pong Game Remix");
+		window.setBounds(300, 300, 400, 150); // experimenting by making the window 200 wider
+		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+		Drawer panel = new Drawer();
+		window.add(panel);
+
+		window.setVisible(true);
 	}
 	
 }
