fd20ad6f141dd89e04d76c313cd7a263481c425c
Hayden Floyd <144164954+hfloyd501@users.noreply.github.com>
2024-05-10 10:16:53 -0700

    aaron

--- fnl_CapstoneProject/src/PongTest.java
+++ fnl_CapstoneProject/src/PongTest.java
@@ -2,178 +2,178 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyListener;
 import java.awt.BorderLayout;
-
 //main class
-public class PongTest extends JPanel implements ActionListener
+public class PongTest extends JPanel implements ActionListener//, KeyListener
 {
 //	private static final String JButton = null;
 	private static JTextField display;
-	JFrame mainwindow = new JFrame();
-	JPanel cpuWindow = new JPanel();
-	private JTextField search;
-	private JTextArea output;
+	
 	private static int WINDOW_WIDTH = 800, WINDOW_HEIGHT = 600;
 	JPanel bottomPanel = new JPanel(new BorderLayout());
 	JPanel topPanel = new JPanel(new BorderLayout());
- 	public static void main(String[] args) 
- 	{
- 		JFrame mainWindow = new JFrame("Pong Game Remix");
- 		mainWindow.setResizable(false);
-		mainWindow.setBounds(150, 50, WINDOW_WIDTH+15, WINDOW_HEIGHT+32); 
-		mainWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		PongTest panel = new PongTest();
+//	static Drawer panel = new Drawer(WINDOW_WIDTH, WINDOW_HEIGHT);
+	static JPanel welcomeScreen;
+	static JFrame mainWindow;
+	static JButton clickMe;
+	JPanel scoreBoard = new JPanel();
+	Drawer panel = new Drawer (1000, 500);
+	
+	public static void main(String[] args)
+	{
 		
-		mainWindow.add(panel);
+		new PongTest();
+	
+	}
+	public PongTest()
+	{
+		mainWindow = new JFrame("Pong Game Remix");
+		mainWindow.setResizable(false);
+		mainWindow.setBounds(150, 50, WINDOW_WIDTH+15, WINDOW_HEIGHT+32);
+		mainWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+//		PongTest panel = new PongTest();		
+//		mainWindow.add(panel);
 		mainWindow.setResizable(true);
 		mainWindow.setVisible(true);
 		
-		
-		
-//		JFrame mainwindow = new JFrame("Pong Game Remix");
-//		mainwindow.setResizable(false);
-//		mainwindow.setBounds(150, 50, 1280, 750); 
-//		mainwindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-//		Drawer panel = new Drawer();
-//		mainwindow.addKeyListener(panel);
-//		mainwindow.add(panel);
-//		JButton newButton = new JButton("Player vs Cpu");
-//		newButton.addActionListener();
-//		mainwindow.add(newButton);
-//		
-//		mainwindow.setVisible(true);
-//		
-//		//timer so that every 30 ms action occurs
-//		Timer timer = new Timer(30, new ActionListener() {
-//			public void actionPerformed(ActionEvent e) {
-//		        panel.run();
-//		        panel.repaint();
-//			}
-//		});
-//		timer.start();
-//		
-//		JFrame cpuWindow = new JFrame("Player vs Cpu");
-//		cpuWindow.setResizable(false);
-//		cpuWindow.setBounds(150, 50, 1280, 750); 
-//		cpuWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-//		Drawer secondPanel = new Drawer();
-//		cpuWindow.addKeyListener(panel);
-//		cpuWindow.add(panel);
-//		cpuWindow.setVisible(false);
-//		
-//		Timer timer1 = new Timer(30, new ActionListener() {
-//			public void actionPerformed(ActionEvent e) {
-//				JButton b = (JButton)e.getSource();
-//				mainwindow.setVisible(false);
-//				cpuWindow.setVisible(true);
-//			}
-//		});
- 		
- 		new PongTest();
- 	
- 	}
- 	public PongTest()
- 	{
- 		
- 		super(new BorderLayout());
-		output = new JTextArea();
-		JScrollPane scroll = new JScrollPane(output);
-		add(scroll, BorderLayout.CENTER);
-		search = new JTextField();
-		search.addActionListener(this);
-		
-		JButton goButton = new JButton("Player vs Cpu");
-		goButton.addActionListener(this);
-		
-//		JPanel bottomPanel = new JPanel(new BorderLayout());
-		bottomPanel.add(goButton, BorderLayout.CENTER);
-		bottomPanel.add(goButton, BorderLayout.EAST);
-		add(bottomPanel, BorderLayout.SOUTH);
-		
-//		JPanel topPanel = new JPanel(new BorderLayout());
-		
-		JLabel label = new JLabel("Welcome to player vs CPU:");
-		topPanel.add(label, BorderLayout.CENTER);
-		add(topPanel, BorderLayout.NORTH);
-		Drawer panel = new Drawer(WINDOW_WIDTH, WINDOW_HEIGHT);
-		topPanel.addKeyListener(panel);
-		topPanel.add(panel, BorderLayout.SOUTH);
-
-
-//		
-//		mainwindow.setVisible(true);
-//		
-		//timer so that every 30 ms action occurs
+		mainWindow.addKeyListener(panel);
 		Timer timer = new Timer(30, new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 		        panel.run();
 		        panel.repaint();
 			}
 		});
-		timer.start();
+		//timer.start();
 		
-		topPanel.setVisible(false);
+		welcomeScreen = new JPanel();
+		welcomeScreen.setBackground(Color.GREEN);
+		welcomeScreen.setBounds(0, 0, 800, 800);
+		JLabel label = new JLabel("Hello and welcome to the Pong Game Remix!");
+		welcomeScreen.add(label);
+		clickMe = new JButton("Press to play against a cpu");
+		clickMe.addActionListener(
+				new ActionListener()
+				{
+					public void actionPerformed(ActionEvent l)
+					{
+						welcomeScreen.setVisible(false);
+						mainWindow.remove(welcomeScreen);
+					
+						mainWindow.add(panel);
+						timer.start();
+					}
+				});
+		welcomeScreen.add(clickMe);
+		welcomeScreen.setVisible(true);
+		
+		mainWindow.add(welcomeScreen);
 		
 		
 		
-// 		JFrame mainwindow = new JFrame("Pong Game Remix");
-//		mainwindow.setResizable(false);
-//		mainwindow.setBounds(150, 50, 1280, 750); 
-//		mainwindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-//		Drawer panel = new Drawer();
-//		mainwindow.addKeyListener(panel);
-//		mainwindow.add(panel);
-//		
-//		JButton newButton = new JButton("Player vs Cpu");
-//		newButton.setBounds(getBounds());
-//		newButton.addActionListener(this);
-//		mainwindow.add(newButton);
-//		
-//		mainwindow.setVisible(true);
-//		
-//		JPanel cpuWindow = new JPanel();
-//		
-//		cpuWindow.setBounds(150, 50, 1280, 750); 
-//		//cpuWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-//		Drawer secondPanel = new Drawer();
-//		cpuWindow.addKeyListener(secondPanel);
-//		mainwindow.add(cpuWindow);
-//		cpuWindow.setVisible(false);
-//		
-//		
-//		//timer so that every 30 ms action occurs
-//		Timer timer = new Timer(30, new ActionListener() {
-//			public void actionPerformed(ActionEvent e) {
-//		        panel.run();
-//		        panel.repaint();
-//		        
-//			}
-//		});
-//		
-//		
-//		timer.start();
-//		
-//		
-//		
-	}
- 	
-
-	@Override
-	public void actionPerformed(ActionEvent k) {
-		// TODO Auto-generated method stub
 		
-		bottomPanel.setVisible(false);
-		topPanel.setVisible(true);
+//		
 		
+// 		Drawer drawer1 = new Drawer();
+// 		int p1sScore = drawer1.getp1Score();
+// 		int cpuScore = drawer1.getcpuScore();
+// 		
+// 		
+// 		
+// 		scoreBoard.setBounds(0, 0, 100, 50);
+// 		
+// 		JPanel p1Score = new JPanel();
+// 		p1Score.setBounds(10, 20, 25, 25);
+// 		p1Score.setBackground(Color.WHITE);
+// 		JLabel actp1Sco = new JLabel("" + p1sScore);
+// 		p1Score.add(actp1Sco);
+// 		scoreBoard.add(p1Score);
+// 		
+// 		JPanel cpusScore = new JPanel();
+// 		cpusScore.setBounds(40, 20, 25, 25);
+// 		cpusScore.setBackground(Color.WHITE);
+// 		JLabel actSco = new JLabel("" + cpuScore);
+// 		cpusScore.add(actSco);
+// 		scoreBoard.add(cpusScore);
+// 		
+// 		
+// 		scoreBoard.setBackground(Color.BLACK);
+		
+		
+		
+		
+		
+		
+		//JPanel welcomeScreen = new JPanel();
+		
+		 		
+// 		welcomeScreen = new JPanel();
+// 		JLabel label = new JLabel("Hello:");
+// 		welcomeScreen.add(label);
+// 		JButton clickMe = new JButton("Press for new screen!");
+// 		welcomeScreen.setVisible(true);
+// 		
+// 		mainWindow.add(welcomeScreen);
+		
+		
+// 		secondScreen = new JPanel();
+// 		JLabel label1 = new JLabel("Hello again!");
+// 		secondScreen.add(label1);
+// 		secondScreen.setVisible(false);
+// 		
+// 		mainWindow.add(secondScreen);
+		
+			
 	}
 	
+//	@Override
+	public void actionPerformed(ActionEvent k) {
+	
+	}
+//		// TODO Auto-generated method stub
+//		
+//		if (k.getSource() == clickMe)
+//		{
+//			welcomeScreen.setVisible(false);
+//			mainWindow.remove(welcomeScreen);
+//			
+//			
+//			
+//
+//			
+//			//mainWindow.add(scoreBoard);
+//			mainWindow.add(panel);
+//			
+//		}
+//		
+//		
+//		
+//		
+//		
+//		bottomPanel.setVisible(false);
+//		
+//		
+////		Timer timer = new Timer(30, new ActionListener() {
+////			public void actionPerformed(ActionEvent e) {
+////		        panel.run();
+////		        panel.repaint();
+////			}
+////		});
+////		timer.start();
+//		
+//	}
+//	
+//	public void actionPerformed(KeyEvent b)
+//	{
+//		
+//	}
+	
+	
 }
-		        
+		       
 			
-
 		
-	    
-
+	   
 		
 		
 	
