0c443846e2760e82b021f8c31b4e15f8a123357a
Hayden Floyd <144164954+hfloyd501@users.noreply.github.com>
2024-05-17 12:18:15 -0700

    rel

--- fnl_CapstoneProject/src/PongTest.java
+++ fnl_CapstoneProject/src/PongTest.java
@@ -14,6 +14,17 @@ public class PongTest extends JPanel implements ActionListener
 	
 		public PongTest() {
 			mainWindow = new JFrame("Pong Game Remix");
+			mainWindow.setBounds(150, 50, WINDOW_WIDTH+15, WINDOW_HEIGHT+82);
+			mainWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+			mainWindow.setVisible(true);
+			mainWindow.setResizable(false);
+			
+			welcomeScreen = new JPanel();
+			welcomeScreen.setBackground(Color.GREEN);
+			welcomeScreen.setBounds(0, 0, WINDOW_WIDTH+15, WINDOW_HEIGHT+82);
+			
+			JLabel label = new JLabel("Welcome To Pong");
+			
 			clickCPU = new JButton("Press to play against a cpu!");
 			clickEasy = new JButton("Easy");  //4 cpu modes
 			clickMedium = new JButton("Medium");
@@ -21,26 +32,20 @@ public class PongTest extends JPanel implements ActionListener
 			clickRandom = new JButton("Random Mode");
 			clickPVP = new JButton("Press to play against your friend!");
 			clickFourPVP = new JButton("Press to play a four paddle game!");
-			mainWindow.setBounds(150, 50, WINDOW_WIDTH+15, WINDOW_HEIGHT+82);
-			mainWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+			
 			easy = new Drawer(WINDOW_WIDTH, WINDOW_HEIGHT, 1);
 			medium = new Drawer(WINDOW_WIDTH, WINDOW_HEIGHT, 2);
 			hard = new Drawer(WINDOW_WIDTH, WINDOW_HEIGHT, 3);
 			random = new Drawer(WINDOW_WIDTH, WINDOW_HEIGHT, 4);
 			PVP = new Drawer(WINDOW_WIDTH, WINDOW_HEIGHT, 5);
 			fourPVP = new Drawer(WINDOW_WIDTH, WINDOW_HEIGHT, 6);
-			mainWindow.setVisible(true);
-			welcomeScreen = new JPanel();
-			welcomeScreen.setBackground(Color.GREEN);
-			welcomeScreen.setBounds(0, 0, WINDOW_WIDTH, WINDOW_HEIGHT);
-			JLabel label = new JLabel("Welcome To Pong");
 			
 			clickCPU.setPreferredSize(new Dimension(250, 100));
 			clickCPU.setBackground(Color.RED);
 			clickPVP.setPreferredSize(new Dimension(300, 100));
 			clickPVP.setBackground(Color.PINK);
-//			clickEasy.setPreferredSize(new Dimension(100, 100));
-//			clickEasy.setBackground(Color.CYAN);
+			clickEasy.setPreferredSize(new Dimension(100, 100));
+			clickEasy.setBackground(Color.CYAN);
 			
 			welcomeScreen.add(label);
 			
@@ -50,9 +55,29 @@ public class PongTest extends JPanel implements ActionListener
 						public void actionPerformed(ActionEvent l)
 						{
 							
+							welcomeScreen.setVisible(false);
+							mainWindow.remove(welcomeScreen);
+						
+							welcomeScreen.setVisible(true);
+							mainWindow.requestFocus();
+							
+							
+							
+							mainWindow.getContentPane().add(BorderLayout.WEST, clickEasy);
+							
+							
+							
 							clickEasy.addActionListener(new ActionListener() { public void actionPerformed(ActionEvent l) { 
+//								mainWindow.remove(welcomeScreen);
+								mainWindow.requestFocus(); 
+								mainWindow.remove(clickMedium);
+								mainWindow.remove(clickEasy);
+								mainWindow.remove(clickHard);
 								welcomeScreen.setVisible(false);
-								mainWindow.remove(welcomeScreen);
+								mainWindow.repaint();
+								mainWindow.addKeyListener(easy);
+								mainWindow.add(easy);
+									
 								Timer timer = new Timer(30, new ActionListener() {
 									public void actionPerformed(ActionEvent e) {
 								        easy.run();
@@ -60,16 +85,22 @@ public class PongTest extends JPanel implements ActionListener
 									}
 								});
 								timer.setInitialDelay(1000);
-								timer.start();
-								mainWindow.addKeyListener(easy);
-								mainWindow.add(easy);
-								welcomeScreen.setVisible(true);
-								mainWindow.requestFocus();
+								timer.start();							
+								
+								mainWindow.setVisible(true);
+								
 							}});
 							
 							clickMedium.addActionListener(new ActionListener() { public void actionPerformed(ActionEvent l) { 
+								mainWindow.requestFocus(); 
+								mainWindow.remove(clickMedium);
+								mainWindow.remove(clickEasy);
+								mainWindow.remove(clickHard);
 								welcomeScreen.setVisible(false);
-								mainWindow.remove(welcomeScreen);
+								mainWindow.repaint();
+								mainWindow.addKeyListener(medium);
+								mainWindow.add(medium);
+									
 								Timer timer = new Timer(30, new ActionListener() {
 									public void actionPerformed(ActionEvent e) {
 								        medium.run();
@@ -77,16 +108,21 @@ public class PongTest extends JPanel implements ActionListener
 									}
 								});
 								timer.setInitialDelay(1000);
-								timer.start();
-								mainWindow.addKeyListener(medium);
-								mainWindow.add(medium);
-								welcomeScreen.setVisible(true);
-								mainWindow.requestFocus();
+								timer.start();							
+								
+								mainWindow.setVisible(true);
 							}});
 							
 							clickHard.addActionListener(new ActionListener() { public void actionPerformed(ActionEvent l) { 
+								mainWindow.requestFocus(); 
+								mainWindow.remove(clickMedium);
+								mainWindow.remove(clickEasy);
+								mainWindow.remove(clickHard);
 								welcomeScreen.setVisible(false);
-								mainWindow.remove(welcomeScreen);
+								mainWindow.repaint();
+								mainWindow.addKeyListener(hard);
+								mainWindow.add(hard);
+									
 								Timer timer = new Timer(30, new ActionListener() {
 									public void actionPerformed(ActionEvent e) {
 								        hard.run();
@@ -94,16 +130,21 @@ public class PongTest extends JPanel implements ActionListener
 									}
 								});
 								timer.setInitialDelay(1000);
-								timer.start();
-								mainWindow.addKeyListener(hard);
-								mainWindow.add(hard);
-								welcomeScreen.setVisible(true);
-								mainWindow.requestFocus();
+								timer.start();							
+								
+								mainWindow.setVisible(true);
 							}});
 							
 							clickRandom.addActionListener(new ActionListener() { public void actionPerformed(ActionEvent l) { 
+								mainWindow.requestFocus(); 
+								mainWindow.remove(clickMedium);
+								mainWindow.remove(clickEasy);
+								mainWindow.remove(clickHard);
 								welcomeScreen.setVisible(false);
-								mainWindow.remove(welcomeScreen);
+								mainWindow.repaint();
+								mainWindow.addKeyListener(random);
+								mainWindow.add(random);
+									
 								Timer timer = new Timer(30, new ActionListener() {
 									public void actionPerformed(ActionEvent e) {
 								        random.run();
@@ -111,14 +152,13 @@ public class PongTest extends JPanel implements ActionListener
 									}
 								});
 								timer.setInitialDelay(1000);
-								timer.start();
-								mainWindow.addKeyListener(random);
-								mainWindow.add(random);
-								welcomeScreen.setVisible(true);
-								mainWindow.requestFocus();
+								timer.start();							
+								
+								mainWindow.setVisible(true);
 							}});
-							
-							
+
+							mainWindow.getContentPane().add(BorderLayout.CENTER, clickMedium);
+							mainWindow.getContentPane().add(BorderLayout.EAST, clickHard);
 							
 						}
 					});
@@ -169,14 +209,15 @@ public class PongTest extends JPanel implements ActionListener
 						}
 					});
 			mainWindow.add(welcomeScreen);
-	
+//	
 			welcomeScreen.add(clickCPU);
-			welcomeScreen.add(clickEasy);
-			welcomeScreen.add(clickMedium);
-			welcomeScreen.add(clickHard);
-			welcomeScreen.add(clickRandom);
+//			welcomeScreen.add(clickEasy);
+//			welcomeScreen.add(clickMedium);
+//			welcomeScreen.add(clickHard);
+//			welcomeScreen.add(clickRandom);
 			welcomeScreen.add(clickPVP);
 			welcomeScreen.add(fourPVP);
+			mainWindow.setVisible(true);
 		}
 	
 	
